90f25271ee4a497e2f388951b8a14e8661ceefba
terlanjurtampan36 <user36@realtechltd.com>
2024-10-16 17:21:19 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
-import music from './upload/music.mp3'
 import MainPages from "./pages/MainPages"
 
 function App() {
-  let audioMusic = new Audio(music)
-  useEffect(() => {
-    audioMusic.play()
-    window.scrollTo(0, 0)
-  }, [])
 
   return (
     <>
--- src/pages/container/RSVPSection.jsx
+++ src/pages/container/RSVPSection.jsx
@@ -9,7 +9,9 @@ export default function RSVPSection() {
         name: '',
         message: '',
         attendance: 'true',
-        guest: '1'
+        guest: '1',
+        church: '',
+        reception: ''
     })
     const [isVisible, setIsVisible] = useState('true')
     const [dataAttendance, setDataAttendance] = useState([])
@@ -18,7 +20,7 @@ export default function RSVPSection() {
         try {
             const { data, error } = await supabase.from('attendance').select('*')
             setDataAttendance(data)
-            setFormData({ name: '', message: '', attendance: 'true', guest: '1' })
+            setFormData({ name: '', message: '', attendance: 'true', guest: '1', church: '', reception: '' })
             setIsVisible('true')
 
             if (error) throw error
@@ -36,9 +38,8 @@ export default function RSVPSection() {
         setFormData((prevData) => ({
             ...prevData,
             [name]: value
+
         }))
-
-
         if (name === 'attendance') {
             value === 'true' ? setIsVisible('true') : setIsVisible('false')
         }
@@ -47,7 +48,14 @@ export default function RSVPSection() {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const { data, error } = await supabase.from('attendance').insert({ name: formData.name, message: formData.message, attendance: formData.attendance, guest: formData.guest })
+            const { data, error } = await supabase.from('attendance').insert({
+                name: formData.name,
+                message: formData.message,
+                attendance: formData.attendance,
+                guest: formData.guest,
+                church: formData.church,
+                reception: formData.reception
+            })
             dataAttendance.push(data)
             toast.success('Thank you for your prayers')
 
@@ -69,40 +77,67 @@ export default function RSVPSection() {
             >
                 Confirmation of Attendance
             </motion.h1>
+            <motion.h1
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className='text-xl text-center my-4 font-parisienne'
+            >
+                See you there
+            </motion.h1>
             <motion.div
-                initial={{ opacity: 0, translateX: 100 }}
-                whileInView={{ opacity: 1, translateX: 1 }}
-                transition={{ duration: 1, delay: 1 }}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 1 }}
                 viewport={{ once: true }}
                 className='p-4 m-4 rounded-2xl'
             >
                 <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
                     <div className="mb-4">
                         <label htmlFor="name" className="block mb-1 text-sm font-medium text-black">Full Name</label>
-                        <input type="name" id="name" name='name' value={formData.name} className="border-b border-gray-300 text-black text-sm block w-full p-2 bg-transparent placeholder:text-black/50 focus-visible:border-none" placeholder="Nama ..." onChange={handleChange} />
+                        <input type="name" id="name" name='name' value={formData.name} className="border-b border-gray-300 text-black text-sm block w-full p-2 bg-transparent placeholder:text-black/50 focus-visible:border-none" placeholder="Name ..." onChange={handleChange} />
                     </div>
                     <div className='mb-4'>
                         <label htmlFor="message" className="block mb-1 text-sm font-medium text-black">Message:</label>
-                        <textarea id="message" name='message' rows="4" value={formData.message} className="border border-gray-300 text-black text-sm rounded-lg block w-full p-2 bg-transparent placeholder:text-black/50" placeholder="Ucapan..." onChange={handleChange}></textarea>
+                        <textarea id="message" name='message' rows="4" value={formData.message} className="border border-gray-300 text-black text-sm rounded-lg block w-full p-2 bg-transparent placeholder:text-black/50" placeholder="Message..." onChange={handleChange}></textarea>
                     </div>
                     <div className='mb-4'>
-                        <label htmlFor="attendance" className="block mb-1 text-sm font-medium text-black">Confirmation of Attendance</label>
-                        <select id="attendance" name='attendance' value={formData.attendance} className="border border-gray-300 text-black text-sm rounded-lg block w-full p-2 bg-transparent placeholder:text-black/50" required onChange={handleChange}>
-                            <option value='true'>Hadir</option>
-                            <option value='false'>Tidak hadir</option>
-                        </select>
+                        <label className="block mb-1 text-sm font-medium text-black">Confirmation of Attendance</label>
+                        <div className="toggle">
+                            <input type="radio" name="attendance" value={'true'} id="attedance" onChange={handleChange} checked={formData.attendance === 'true'} />
+                            <label htmlFor="attedance">Hadir</label>
+                            <input type="radio" name="attendance" value={'false'} id="notattendance" onChange={handleChange} checked={formData.attendance === 'false'} />
+                            <label htmlFor="notattendance">Tidak Hadir</label>
+                        </div>
                     </div>
                     {isVisible === 'true' ? (
-                        <div className='mb-4'>
-                            <label htmlFor="guest" className="block mb-1 text-sm font-medium text-black">Guests</label>
-                            <select id="guest" name='guest' value={formData.guest} className="border border-gray-300 text-black text-sm rounded-lg block w-full p-2 bg-transparent placeholder:text-black/50" required onChange={handleChange}>
-                                <option value="1">1 guest</option>
-                                <option value="2">2 guests</option>
-                            </select>
-                        </div>
+                        <motion.div
+                            initial={{ opacity: 0, translateY: -100 }}
+                            animate={{ opacity: 1, translateY: 1 }}
+                            transition={{ duration: .5 }}
+                            viewport={{ once: true }}
+                        >
+                            <div className='my-8'>
+                                <div className='my-4'>
+                                    <input type="checkbox" name="church" id="church" className='mr-2' onChange={handleChange} />
+                                    <label htmlFor="church" className='text-xl'>Holy Matrimony</label>
+                                </div>
+                                <div className='my-4'>
+                                    <input type="checkbox" name="reception" id="reception" className='mr-2' onChange={handleChange} />
+                                    <label htmlFor="reception" className='text-xl'>Wedding Reception</label>
+                                </div>
+                            </div>
+                            <div className='mb-4'>
+                                <label htmlFor="guest" className="block mb-1 text-sm font-medium text-black">Guests</label>
+                                <select id="guest" name='guest' value={formData.guest} className="border border-gray-300 text-black text-sm rounded-lg block w-full p-2 bg-transparent placeholder:text-black/50" required onChange={handleChange}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                        </motion.div>
                     ) : null}
                     <div className='my-8 mx-[20%]'>
-                        <button type="submit" className="text-white hover:text-black bg-black hover:bg-white border border-black/40 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-12 py-3 text-center duration-150 ease-in-out" onClick={handleSubmit}>Submit</button>
+                        <button type="submit" className="text-white hover:text-black bg-[#2b2b2b] hover:bg-white border border-black/40 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-12 py-3 text-center duration-150 ease-in-out" onClick={handleSubmit}>Submit</button>
                     </div>
                 </form>
 
